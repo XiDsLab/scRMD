9e8452a683d184115b2d0b5ac0180f2ee5052a02
Chong Chen <31962998+ChongC1990@users.noreply.github.com>
2019-01-01 21:13:51 +0800

    Create AdjustedRandIndex.R

---
+++ R/AdjustedRandIndex.R
@@ -0,0 +1,29 @@
+MatrixFusion <- function(true_lab,pre_lab){
+  # true_lab is the true community label
+  # pre_lab is the predicted community label
+  r = max(true_lab)
+  s = max(pre_lab)
+  A = matrix(0,r,s)
+  for (i in 1:r){
+    for(j in 1:s){
+      A[i,j] = length(intersect(which(true_lab==i),which(pre_lab==j)))
+    }
+  }
+  return(A)
+}
+
+
+CalculateARI <- function(true_lab,pre_lab){
+  # true_lab is the true community label
+  # pre_lab is the predicted community label
+  # return the adjusted rand index
+  A = MatrixFusion(true_lab,pre_lab)
+  a = apply(A,1,sum)
+  b = apply(A,2,sum)
+  n = length(true_lab)
+  c1 = sum(a*(a-1))/2
+  c2 = sum(b*(b-1))/2
+  c0 = sum(A*(A-1))/2
+  ari = (n*(n-1)*c0/2-c1*c2)/(n*(n-1)*(c1+c2)/4-c1*c2)
+  return(ari)
+}
