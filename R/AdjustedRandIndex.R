d720b74c85a5dc661e3e6ce1a45c780c67304501
Chong Chen <31962998+ChongC1990@users.noreply.github.com>
2019-01-01 21:28:55 +0800

    update

--- R/AdjustedRandIndex.R
+++ R/AdjustedRandIndex.R
@@ -1,17 +1,3 @@
-MatrixFusion <- function(true_lab,pre_lab){
-  # true_lab is the true community label
-  # pre_lab is the predicted community label
-  r = max(true_lab)
-  s = max(pre_lab)
-  A = matrix(0,r,s)
-  for (i in 1:r){
-    for(j in 1:s){
-      A[i,j] = length(intersect(which(true_lab==i),which(pre_lab==j)))
-    }
-  }
-  return(A)
-}
-
 
 CalculateARI <- function(true_lab,pre_lab){
   # true_lab is the true community label
